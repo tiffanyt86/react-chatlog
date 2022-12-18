ce8c1d2caa2e9e457732a846bbd521c0a34003b4
Tiffany Tran <tif.tran86@gmail.com>
2022-12-17 17:20:07 -0800

    passed wave 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -11,6 +12,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={chatMessages[0].sender}
+          body={chatMessages[0].body}
+          timeStamp={chatMessages[0].timeStamp}
+        />
       </main>
     </div>
   );
